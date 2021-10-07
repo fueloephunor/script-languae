c1a33cab41c3a6741c54f6c789faa803b61ea74e
fulophunor <fueloephunor@gmail.com>
2021-10-07 14:39:41 +0200

    elso program

---
+++ first.py
@@ -0,0 +1,9 @@
+print("Pls give your name")
+name = input()
+
+print("Pls give your age")
+age = int(input())
+
+print("Your name: {0}, age: {1}".format(name, age))
+print("Next year you will be {0} year old".format(age + 1))
+
