6793f7abcde5f14368e94660484b9815e77f44ba
fulophunor <fueloephunor@gmail.com>
2021-10-07 14:44:49 +0200

    calculator introduced

---
+++ secret_logic.py
@@ -0,0 +1,17 @@
+def is_numaric(text):
+    return text.isnumaric()
+
+def is_supperd_operator(text):
+    return text in ['+', '-', '*', '/']
+
+def calculate(operand1, p_operator, operand2):
+    rv = 0
+    if p_operator == "+":
+        rv = operand1 + operand2
+    elif p_operator == "-":
+        rv = operand1 - operand2
+    elif p_operator == "*":
+        rv = operand1 * operand2
+    elif p_operator == "/":
+        rv = operand1 / operand2
+    return rv
